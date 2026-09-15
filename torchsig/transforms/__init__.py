d9f992e539ccebaa02e1a5204626edfc2cb76ad7
ajpotts <ajpotts@users.noreply.github.com>
2026-09-15 08:05:24 -0400

    Add rf_channel_augmentation

--- torchsig/transforms/__init__.py
+++ torchsig/transforms/__init__.py
@@ -1,3 +1,3 @@
-from . import base_transforms, functional, impairments, metadata_transforms, transforms
+from . import base_transforms, functional, impairments, metadata_transforms, rf_channel, transforms
 
-__all__ = ["base_transforms", "functional", "impairments", "metadata_transforms", "transforms"]
+__all__ = ["base_transforms", "functional", "impairments", "metadata_transforms", "rf_channel", "transforms"]
